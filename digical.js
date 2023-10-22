f576bba069955f93f429c87762305e774f58e5b8
RobertoRedes2001 <romaav@floridauniversitaria.es>
2023-10-22 15:47:10 +0200

    Calculadora Funcional, codigo corregido

--- digical.js
+++ digical.js
@@ -1,4 +1,4 @@
-function calcularDmgAtributo(atributoAtacante, atributoDefensor) {
+function calcularDmgAtributo(atkAtacante,atributoAtacante, atributoDefensor) {
     //Superioridad de cada atributo contra otro
     let bonos = {
         'virus': { 'vacuna': -0.5, 'data': -0.5 },
@@ -9,7 +9,7 @@ function calcularDmgAtributo(atributoAtacante, atributoDefensor) {
     //Calculamos si el ataque hace menos o más daño por su atributo
     //en caso de que no haya coincidencia retornamos bono 0.
     if (bonos[atributoAtacante] && bonos[atributoAtacante][atributoDefensor]) {
-        return Math.round(atributoAtacante * bonos[atributoAtacante][atributoDefensor]);
+        return Math.round(atkAtacante * bonos[atributoAtacante][atributoDefensor]);
     }else{
         return 0;
     }
@@ -28,7 +28,6 @@ function calcularDmgTipo(atkAtacante, tipoAtaque, tipoDefensor) {
         'luz' : {'oscuridad' : 0.5},
         'oscuridad' : {'luz' : 0.5},
     };
-
     //Calculamos si el ataque hace menos o más daño por su tipo, 
     //en caso de que no haya coincidencia retornamos bono 0.
     if (bonos[tipoAtaque] && bonos[tipoAtaque][tipoDefensor]) {
@@ -55,7 +54,7 @@ function calcularDmgDef(defDefensor,bonodef,buff,debuff){
     buff ? defDefensor = defDefensor*1.25 : defDefensor;
     debuff ? defDefensor = defDefensor-(defDefensor*0.25) : defDefensor;
     //Retornamos la suma del stat de DEF con el bono
-    return Math.round(def + bonodef);
+    return Math.round(defDefensor + bonodef);
 }
 
 function calcularTotalDmg(ataque,defensa,criticoPrecision,cubrirse, boost){
@@ -71,9 +70,76 @@ function calcularTotalDmg(ataque,defensa,criticoPrecision,cubrirse, boost){
     }
 }
 
-let tipos = [
-    'fuego','agua','planta','tierra','electrico','viento', 'luz', 'oscuridad'
-];
+// Encapsula los elementos <input> y <input type="checkbox> del grupo ATK
+let atkInput = document.getElementById('atk');
+let bonoATKInput = document.getElementById('bonoATK');
+let tiposSelectATK = document.getElementById('tipoATK');
+let atributosSelectATK = document.getElementById('atributoATK');
+let chargeATKCheckbox = document.getElementById('chargeATK');
+let breakATKCheckbox = document.getElementById('breakATK');
+let boostCheckbox = document.getElementById('boost');
+
+// Encapsula los elementos <input> y <input type="checkbox> del grupo DEF
+let defInput = document.getElementById('def');
+let bonoDEFInput = document.getElementById('bonoDEF');
+let tiposSelectDEF = document.getElementById('tipoDEF');
+let atributosSelectDEF = document.getElementById('atributoDEF');
+let chargeDEFCheckbox = document.getElementById('chargeDEF');
+let breakDEFCheckbox = document.getElementById('breakDEF');
+let guardCheckbox = document.getElementById('guard');
+
+// Encapsula los elementos <input> y <input type="checkbox> del grupo DADOS
+let dadoInput = document.getElementById('dado');
+let critDMGCheckbox = document.getElementById('criDMG');
+let critSPDCheckbox = document.getElementById('critSPD');
+
+//Cargamos las listas de atributos y tipos
+document.addEventListener('DOMContentLoaded', function () {
+  
+    let selectType1 = document.getElementById('tipoATK');
+    let selectAtribute1 = document.getElementById('atributoATK');
+    let selectType2 = document.getElementById('tipoDEF');
+    let selectAtribute2 = document.getElementById('atributoDEF');
+
+    // Array de tipos
+    let tipos = ['fuego', 'agua', 'planta', 'tierra', 'electrico', 'viento', 'luz', 'oscuridad', 'neutral'];
+
+    // Array de atributos
+    let atributos = ['vacuna', 'data', 'virus', 'free'];
+
+    // Función para agregar opciones a un elemento <select>
+    function agregarOpcionesASelect(select, opciones) {
+        opciones.forEach(function (opcion) {
+            let option = document.createElement('option');
+            option.text = opcion;
+            select.add(option);
+        });
+    }
+
+    // Agregar opciones a los elementos <select>
+    agregarOpcionesASelect(selectType1, tipos);
+    agregarOpcionesASelect(selectAtribute1, atributos);
+    agregarOpcionesASelect(selectType2, tipos);
+    agregarOpcionesASelect(selectAtribute2, atributos);
+});
+
+document.getElementsByTagName("button")[0].addEventListener("click",function(){
+    
+    let tipo = calcularDmgTipo(parseInt(atkInput.value),tiposSelectATK.value,tiposSelectDEF.value);
+    let atributo = calcularDmgAtributo(parseInt(atkInput.value),atributosSelectATK.value,atributosSelectDEF.value);
+    let totalATK = calcularDmgAtk(parseInt(atkInput.value),parseInt(dadoInput.value),tipo,atributo,
+    parseInt(bonoATKInput.value),critDMGCheckbox.checked,chargeATKCheckbox.checked,breakATKCheckbox.checked);
+
+    let totalDEF = calcularDmgDef(parseInt(defInput.value),parseInt(bonoDEFInput.value),chargeDEFCheckbox.checked,breakDEFCheckbox.checked);
+    let calculoFinal = calcularTotalDmg(totalATK,totalDEF,critSPDCheckbox.checked,guardCheckbox.checked,boostCheckbox.checked);
+
+    document.getElementsByTagName("td")[0].textContent = totalATK;
+    document.getElementsByTagName("td")[2].textContent = totalDEF;
+    document.getElementsByTagName("td")[4].textContent = calculoFinal;
+})
+
+
+
 
 
 
